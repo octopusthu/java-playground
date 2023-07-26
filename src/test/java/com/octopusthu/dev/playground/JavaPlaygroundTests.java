9dd3c71469d0a75d1cc023ccef138df499695b63
octopusthu <octopusthu@gmail.com>
2023-07-26 10:11:45 +0800

    Add ifElsePrintTest

--- src/test/java/com/octopusthu/dev/playground/JavaPlaygroundTests.java
+++ src/test/java/com/octopusthu/dev/playground/JavaPlaygroundTests.java
@@ -1,7 +1,10 @@
 package com.octopusthu.dev.playground;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 public class JavaPlaygroundTests {
@@ -13,5 +16,17 @@ public class JavaPlaygroundTests {
                 System.out.println("Unexpected!");
             }
         });
+    }
+
+    @Test
+    void ifElsePrintTest() {
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        if (System.out.printf("A") == null) {
+            System.out.print("A");
+        } else {
+            System.out.print("B");
+        }
+        assertEquals("AB", outputStreamCaptor.toString().trim());
     }
 }
