36bcb0b78daf3ec25e2ab36409c7a59753b4413f
octopusthu <octopusthu@gmail.com>
2023-12-05 20:28:46 +0800

    Updates

---
+++ src/main/java/com/octopusthu/dev/exercises/RemoveArrayElement.java
@@ -0,0 +1,18 @@
+package com.octopusthu.dev.exercises;
+
+/**
+ * arr = [3,2,1,3], val = 3<br>
+ * =><br>
+ * 2, arr = [2,1,any,any]<br>
+ * or<br>
+ * 2, arr = [1,2,any,any]<br>
+ * <p><br>
+ * Space complexity must be O(1)
+ */
+public class RemoveArrayElement {
+    public static void main(String[] args) {
+    }
+
+    void removeArrayElement(int[] arr, int val) {
+    }
+}
