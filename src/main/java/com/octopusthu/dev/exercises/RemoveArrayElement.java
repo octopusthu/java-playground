f8cd3cafab0682ee524528ea60bc04de0a96f3dd
octopusthu <octopusthu@gmail.com>
2023-12-06 23:14:10 +0800

    Finished RemoveArrayElement exercise

--- src/main/java/com/octopusthu/dev/exercises/RemoveArrayElement.java
+++ src/main/java/com/octopusthu/dev/exercises/RemoveArrayElement.java
@@ -10,9 +10,29 @@ package com.octopusthu.dev.exercises;
  * Space complexity must be O(1)
  */
 public class RemoveArrayElement {
-    public static void main(String[] args) {
-    }
-
-    void removeArrayElement(int[] arr, int val) {
+    int removeArrayElement(int[] arr, int val) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int p1 = 0, p2 = arr.length - 1;
+        while (p1 < p2) {
+            if (arr[p1] == val) {
+                while (p1 < p2) {
+                    if (arr[p2] == val) {
+                        p2--;
+                        continue;
+                    }
+                    arr[p1] = arr[p1] + arr[p2];
+                    arr[p2] = arr[p1] - arr[p2];
+                    arr[p1] = arr[p1] - arr[p2];
+                    p1++;
+                    p2--;
+                }
+            } else {
+                p1++;
+            }
+        }
+        // p1 == p2 or p1 == p2 + 1
+        return arr[p1] == val ? p1 : p1 + 1;
     }
 }
---
+++ src/test/java/com/octopusthu/dev/exercises/RemoveArrayElementTest.java
@@ -0,0 +1,57 @@
+package com.octopusthu.dev.exercises;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RemoveArrayElementTest {
+    final RemoveArrayElement target = new RemoveArrayElement();
+    static Object[][] testData;
+
+    @BeforeAll
+    static void init() {
+        testData = new Object[][]{
+            new Object[]{new int[]{1, 2, 3}, 1, List.of(2, 3)},
+            new Object[]{new int[]{3, 2, 1, 3}, 3, List.of(2, 1)},
+            new Object[]{new int[]{2, 2, 1, 1}, 2, List.of(1, 1)},
+            new Object[]{new int[]{1, 1, 2, 2}, 2, List.of(1, 1)},
+            new Object[]{new int[]{}, 1, List.of()},
+            new Object[]{new int[]{1}, 1, List.of()},
+            new Object[]{new int[]{1}, 2, List.of(1)},
+            new Object[]{new int[]{1, 2}, 1, List.of(2)},
+            new Object[]{new int[]{2, 1}, 1, List.of(2)},
+            new Object[]{new int[]{1, 1, 1}, 1, List.of()},
+        };
+    }
+
+    @Test
+    void testRemoveArrayElement() {
+        for (Object[] data : testData) {
+            int[] arr = (int[]) data[0];
+            arr = Arrays.copyOf(arr, arr.length);
+            int val = (int) data[1];
+            @SuppressWarnings("unchecked")
+            List<Integer> expected = (List<Integer>) data[2];
+            int remained = target.removeArrayElement(arr, val);
+            assertResult(arr, remained, expected);
+        }
+    }
+
+    void assertResult(int[] arr, int remained, List<Integer> expected) {
+        expected = new ArrayList<>(expected);
+        Assertions.assertEquals(expected.size(), remained);
+        for (int i = 0; i < remained; i++) {
+            if (!expected.remove((Object) arr[i])) {
+                Assertions.fail(String.format("unexpected remaining element: %d", arr[i]));
+            }
+        }
+        if (!expected.isEmpty()) {
+            Assertions.fail("expected elements not present in remained array: " + Arrays.toString(
+                expected.toArray()));
+        }
+    }
+
+}
